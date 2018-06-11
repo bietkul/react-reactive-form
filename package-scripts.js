df44b464822da291695bf1b7af008e3aaa51e5a6
bietkul <kuldeep@sahusoft.com>
2018-06-11 18:46:44 +0530

    Build Fixes

---
+++ package-scripts.js
@@ -0,0 +1,59 @@
+const npsUtils = require('nps-utils')
+
+const series = npsUtils.series
+const concurrent = npsUtils.concurrent
+const rimraf = npsUtils.rimraf
+const crossEnv = npsUtils.crossEnv
+
+module.exports = {
+  scripts: {
+    test: {
+      default: crossEnv('NODE_ENV=test')
+    },
+    clean: {
+      description: 'delete the build folders',
+      script: 'rimraf lib dist es'
+    },
+    build: {
+      description: 'delete the dist directory and run all builds',
+      default: series(
+        rimraf('dist'),
+        concurrent.nps(
+          'build.es',
+          'build.cjs',
+          'build.umd.main',
+          'build.umd.min',
+          'copyTypes'
+        )
+      ),
+      es: {
+        description: 'run the build with rollup (uses rollup.config.js)',
+        script: 'rollup --config --environment FORMAT:es'
+      },
+      cjs: {
+        description: 'run rollup build with CommonJS format',
+        script: 'rollup --config --environment FORMAT:cjs'
+      },
+      umd: {
+        min: {
+          description: 'run the rollup build with sourcemaps',
+          script: 'rollup --config --sourcemap --environment MINIFY,FORMAT:umd'
+        },
+        main: {
+          description: 'builds the cjs and umd files',
+          script: 'rollup --config --sourcemap --environment FORMAT:umd'
+        }
+      },
+      andTest: series.nps('build')
+    },
+    copyTypes: series(npsUtils.copy('*.d.ts dist')),
+    validate: {
+      description:
+        'builds the package',
+      default: concurrent.nps('build.andTest', 'test')
+    }
+  },
+  options: {
+    silent: false
+  }
+}
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,67 @@
 // rollup.config.js
+import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
+import { uglify } from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
+
+const minify = process.env.MINIFY
+const format = process.env.FORMAT
+const es = format === 'es'
+const umd = format === 'umd'
+const cjs = format === 'cjs'
+
+let output
+
+if (es) {
+  output = { file: `dist/react-reactive-form.es.js`, format: 'es' }
+} else if (umd) {
+  if (minify) {
+    output = {
+      file: `dist/react-reactive-form.umd.min.js`,
+      format: 'umd'
+    }
+  } else {
+    output = { file: `dist/react-reactive-form.umd.js`, format: 'umd' }
+  }
+} else if (cjs) {
+  output = { file: `dist/react-reactive-form.cjs.js`, format: 'cjs' }
+} else if (format) {
+  throw new Error(`invalid format specified: "${format}".`)
+} else {
+  throw new Error('no format specified. --environment FORMAT:xxx')
+}
+
 export default {
-  dest: 'dist/main.js',
-  entry: 'src/lib.js',
-  format: 'cjs',
-  plugins: [resolve(), commonjs(), babel({
-    exclude: 'node_modules/**',
-    plugins: ['external-helpers']
-  })]
+  input: 'src/index.js',
+  output: Object.assign(
+    {
+      name: 'react-reactive-form',
+      exports: 'named',
+      globals: {
+        react: 'React',
+        'prop-types': 'PropTypes'
+      }
+    },
+    output
+  ),
+  external: ['react', 'prop-types'],
+  plugins: [
+    resolve({ jsnext: true, main: true }),
+    commonjs({ include: 'node_modules/**' }),
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      presets: [['env', { modules: false }], 'stage-2'],
+      plugins: ['external-helpers']
+    }),
+    umd
+      ? replace({
+          'process.env.NODE_ENV': JSON.stringify(
+            minify ? 'production' : 'development'
+          )
+        })
+      : null,
+    minify ? uglify() : null
+  ].filter(Boolean)
 }
