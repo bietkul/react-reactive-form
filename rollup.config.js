fdce3b5cb2cac234c4bc90022ce32c50b827c5ca
bietkul <kuldepsaxena155@gmail.com>
2019-10-08 09:38:01 +0530

    build: v1.0.32

--- rollup.config.js
+++ rollup.config.js
@@ -1,67 +1,67 @@
 // rollup.config.js
-import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import { uglify } from 'rollup-plugin-uglify'
-import replace from 'rollup-plugin-replace'
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import { uglify } from "rollup-plugin-uglify";
+import replace from "rollup-plugin-replace";
 
-const minify = process.env.MINIFY
-const format = process.env.FORMAT
-const es = format === 'es'
-const umd = format === 'umd'
-const cjs = format === 'cjs'
+const minify = process.env.MINIFY;
+const format = process.env.FORMAT;
+const es = format === "es";
+const umd = format === "umd";
+const cjs = format === "cjs";
 
-let output
+let output;
 
 if (es) {
-  output = { file: `dist/react-reactive-form.es.js`, format: 'es' }
+  output = { file: `dist/react-reactive-form.es.js`, format: "es" };
 } else if (umd) {
   if (minify) {
     output = {
       file: `dist/react-reactive-form.umd.min.js`,
-      format: 'umd'
-    }
+      format: "umd"
+    };
   } else {
-    output = { file: `dist/react-reactive-form.umd.js`, format: 'umd' }
+    output = { file: `dist/react-reactive-form.umd.js`, format: "umd" };
   }
 } else if (cjs) {
-  output = { file: `dist/react-reactive-form.cjs.js`, format: 'cjs' }
+  output = { file: `dist/react-reactive-form.cjs.js`, format: "cjs" };
 } else if (format) {
-  throw new Error(`invalid format specified: "${format}".`)
+  throw new Error(`invalid format specified: "${format}".`);
 } else {
-  throw new Error('no format specified. --environment FORMAT:xxx')
+  throw new Error("no format specified. --environment FORMAT:xxx");
 }
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: Object.assign(
     {
-      name: 'react-reactive-form',
-      exports: 'named',
+      name: umd ? "ReactiveForm" : "react-reactive-form",
+      exports: "named",
       globals: {
-        react: 'React',
-        'prop-types': 'PropTypes'
+        react: "React",
+        "prop-types": "PropTypes"
       }
     },
     output
   ),
-  external: ['react', 'prop-types'],
+  external: ["react", "prop-types"],
   plugins: [
-    resolve({ jsnext: true, main: true }),
-    commonjs({ include: 'node_modules/**' }),
+    resolve({ jsnext: true, main: true, browser: true }),
+    commonjs({ include: "node_modules/**" }),
     babel({
-      exclude: 'node_modules/**',
+      exclude: "node_modules/**",
       babelrc: false,
-      presets: [['env', { modules: false }], 'stage-2'],
-      plugins: ['external-helpers']
+      presets: [["env", { modules: false }], "stage-2"],
+      plugins: ["external-helpers"]
     }),
     umd
       ? replace({
-          'process.env.NODE_ENV': JSON.stringify(
-            minify ? 'production' : 'development'
+          "process.env.NODE_ENV": JSON.stringify(
+            minify ? "production" : "development"
           )
         })
       : null,
     minify ? uglify() : null
   ].filter(Boolean)
-}
+};
